bb979ab1ba183e4efc94f6bb503a71aeb04bd0ae
Abraheem97 <abraheem.asad@devsinc.com>
2020-02-11 14:46:08 +0500

    Added styling to the site

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-    has_attached_file :image, :styles => { :medium => "100%x200>" }
+    has_attached_file :image, :styles => { :medium => "250x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :author
 end
