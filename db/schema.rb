71bffa6544ba0af0476feeded8f878a8266600dd
Abraheem97 <abraheem.asad@devsinc.com>
2020-02-14 09:34:21 +0500

    Added images and description to Author

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_111828) do
+ActiveRecord::Schema.define(version: 2020_02_14_040659) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
+    t.text "description"
   end
 
   create_table "average_caches", force: :cascade do |t|
