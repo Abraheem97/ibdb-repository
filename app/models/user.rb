e3e775216d08a975db3295c3cdfb55ddbaa4da7f
Abraheem97 <abraheem.asad@devsinc.com>
2020-02-14 10:01:42 +0500

    Added avatars for users at signup and show them in comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy 
   has_many :comments, dependent: :destroy
 
+  has_attached_file :avatar, styles: { medium: "100x100>", thumb: "59x59>" }, default_url: "/assets/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 
   def username
     return email.split('@').first
